d2d4b4368fa2bad320ede39917ad235419eb4613
230019881 <230019881@STLRC063>
2024-03-19 13:39:44 +0800

    加入專案檔案。

---
+++ 4522project_UIDESIGN/order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4522project_UIDESIGN
+{
+    public partial class order : Form
+    {
+        public order()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void order_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
